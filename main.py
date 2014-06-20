d458198a4068b33600624129a335c69feb5481b8
minakhan01 <mina_udacity@Minas-MacBook-Air.local>
2014-06-20 14:40:12 -0700

    updating mainhandler

--- main.py
+++ main.py
@@ -61,18 +61,7 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        self.response.write('Hello world!')
         self.render("home.html")
-        user = users.get_current_user()
-        if user:
-            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-                        (user.nickname(), users.create_logout_url('/')))
-        else:
-            greeting = ('<a href="%s">Sign in or register</a>.' %
-                        users.create_login_url('/'))
-
-        self.response.out.write("<html><body>%s</body></html>" % greeting)
-
 
 class ProfileHandler(BaseHandler):
     def get(self):
